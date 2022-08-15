0b0584829009c013a9c08bede44e7ca562681882
Son Giang <stakashi1311@gmail.com>
2022-08-16 02:32:12 +0930

    change url of background image

--- src/rollingDice/ExerciseRollingDice.js
+++ src/rollingDice/ExerciseRollingDice.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 import DisplayResult from './DisplayResult'
 import Options from './Options'
+import background from '/rollDice/bgGame.png'
 
 
 export default class ExerciseRollingDice extends Component {
     render() {
         return (
-            <div className="container-fluid d-flex flex-column align-items-center" style={{ backgroundImage: 'url("/rollDice/bgGame.png")', height: 1000, fontSize: 40 }}>
+            <div className="container-fluid d-flex flex-column align-items-center" style={{ backgroundImage: 'url("sicbo-app/rollDice/bgGame.png")', height: 1000, fontSize: 40 }}>
                 <h2 className='text-center' style={{ fontSize: 100, padding: 20 }}>Welcome to Rolling Dice game </h2>
                 <Options />
                 <DisplayResult />
